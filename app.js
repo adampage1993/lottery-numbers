dab8e68fafae779e67e90ad10e09e222d4ff1e06
Adam Page <adampage1993@gmail.com>
2021-01-20 08:49:33 +0000

    Adding logic to random message generator

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ const randomMessages = {
   secondMessages: [5, 6, 7, 8],
   thirdMessages: [9, 10, 11, 12],
 };
+
+const getMessage = () => {
+  const message1 = randomMessages.firstMessages[Math.floor(Math.random() * randomMessages.firstMessages.length)];
+  const message2 = randomMessages.secondMessages[Math.floor(Math.random() * randomMessages.secondMessages.length)];
+  const message3 = randomMessages.thirdMessages[Math.floor(Math.random() * randomMessages.thirdMessages.length)];
+
+  console.log(`Your first message is ${message1}.`)
+  console.log(`Your second message is ${message2}.`)
+  console.log(`Your third message is ${message3}.`)
+}
+getMessage();
